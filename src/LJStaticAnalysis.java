2ceec46e87df5cb7fa4a64419beb837699ecccb4
Zamua Nasrawt <zamua2012@gmail.com>
2016-12-22 01:31:09 -0500

    Add skeleton for future Static Analysis visitor

---
+++ src/LJStaticAnalysis.java
@@ -0,0 +1,56 @@
+public class LJStaticAnalysis extends LJBaseListener {
+    public LJStaticAnalysis() {
+    }
+
+    @Override
+    public void exitProgram(LJParser.ProgramContext ctx) {
+    }
+
+    @Override
+    public void exitFunction(LJParser.FunctionContext ctx) {
+    }
+
+    @Override
+    public void enterBlock(LJParser.BlockContext ctx) {
+    }
+
+    @Override
+    public void exitBlock(LJParser.BlockContext ctx) {
+    }
+
+    @Override
+    public void exitAssignment(LJParser.AssignmentContext ctx) {
+    }
+
+    @Override
+    public void exitConditional(LJParser.ConditionalContext ctx) {
+    }
+
+    @Override
+    public void exitWhile(LJParser.WhileContext ctx) {
+    }
+
+    @Override
+    public void exitReturn(LJParser.ReturnContext ctx) {
+    }
+
+    @Override
+    public void exitBreak(LJParser.BreakContext ctx) {
+    }
+
+    @Override
+    public void exitContinue(LJParser.ContinueContext ctx) {
+    }
+
+    @Override
+    public void exitTest(LJParser.TestContext ctx) {
+    }
+
+    @Override
+    public void exitBinExpr(LJParser.BinExprContext ctx) {
+    }
+
+    @Override
+    public void exitLit(LJParser.LitContext ctx) {
+    }
+}
