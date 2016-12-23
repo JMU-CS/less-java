374759af5544e217ad60935e5123ced9d4d3ea43
Zamua Nasrawt <zamua2012@gmail.com>
2016-12-23 18:26:36 -0500

    Add skeleton for visitor that assigns types

---
+++ src/LJASTAssignTypes.java
@@ -0,0 +1,48 @@
+
+public class LJASTAssignTypes extends LJBaseASTVisitor {
+
+    private ASTNode.DataType functionReturnType;
+    private ASTFunction currentFunction;
+
+    public LJASTAssignTypes() {
+    }
+
+    @Override
+    public void preVisit(ASTFunction node) {
+        currentFunction = node;
+    }
+
+    @Override
+    public void postVisit(ASTReturn node) {
+        currentFunction.returnType = evalExprType(node.value);
+    }
+
+    public ASTNode.DataType evalExprType(ASTExpression expr) {
+        ASTNode.DataType type;
+
+        if (expr instanceof ASTBinaryExpr) {
+           type = evalExprType(expr); 
+        } else if(expr instanceof ASTLocation) {
+           type = evalExprType(expr); 
+        } else if(expr instanceof ASTLiteral) {
+           type = evalExprType(expr); 
+        } else {
+           type = null; 
+        }
+
+        return type;
+    }
+
+    public ASTNode.DataType evalExprType(ASTBinaryExpr expr) {
+        return null;
+    }
+
+    public ASTNode.DataType evalExprType(ASTLocation expr) {
+        return null;
+    }
+
+    public ASTNode.DataType evalExprType(ASTLiteral expr) {
+        return null;
+    }
+}
+
