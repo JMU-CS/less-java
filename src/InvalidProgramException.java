2685144aeaf61fccfdba06cceadb3a21bd4e59af
Zamua Nasrawt <zamua2012@gmail.com>
2016-12-23 19:19:51 -0500

    Copy over from Decaf compiler

---
+++ src/InvalidProgramException.java
@@ -0,0 +1,12 @@
+/**
+ * Error encountered during static/semantic analysis.
+ */
+public class InvalidProgramException extends Exception
+{
+    public static final long serialVersionUID = 1L;
+
+    public InvalidProgramException(String msg)
+    {
+        super(msg);
+    }
+}
