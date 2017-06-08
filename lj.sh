54c40b47b3949d9cff236474b494ee2424f5f579
Zamua Nasrawt <nasrawzo@dukes.jmu.edu>
2017-06-07 20:15:19 -0700

    lowercase 'u'. exit -> exit 1

--- lj.sh
+++ lj.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage: ./lj.sh <file>"
-    exit
+    echo "usage: ./lj.sh <file>"
+    exit 1
 fi
 
 gradle run -Ptestfile="$@" && echo "output:" && cat compiler.out
