44c2bc063b88fe070956b8181cb66fae553ec1ec
Mike Lam <lam2mo@jmu.edu>
2020-02-21 12:00:10 -0500

    Integrate UI into new streamlined build system
    This is still broken at the moment because the wrappers aren't being
    properly integrated for some reason.

--- distro.sh
+++ distro.sh
@@ -2,5 +2,11 @@
 
 GRADLE_WRAPPER="./gradlew"
 
+"$GRADLE_WRAPPER" clean
+
 "$GRADLE_WRAPPER" fatJar
-cp build/libs/*-all.jar distribution/lj.jar
+cp build/libs/lj-all.jar distribution/lj.jar
+
+"$GRADLE_WRAPPER" ui
+cp build/libs/lj-ui.jar distribution/lj-ui.jar
+
--- ui.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-GRADLE_WRAPPER="./gradlew"
-
-"$GRADLE_WRAPPER" ui
-cp build/libs/lj-ui.jar distribution/lj-ui.jar
-java -jar distribution/lj-ui.jar
