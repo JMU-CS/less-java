ad1fe409ce28d5bc29ad89721245d665b3a6a69c
Mike Lam <lam2mo@jmu.edu>
2020-02-28 12:08:26 -0500

    Copy JUnit jarfile to distribution folder

--- distro.sh
+++ distro.sh
@@ -6,6 +6,7 @@ GRADLE_WRAPPER="./gradlew"
 
 "$GRADLE_WRAPPER" fatJar
 cp build/libs/lj-all.jar distribution/lj.jar
+cp libs/*.jar distribution/
 
 "$GRADLE_WRAPPER" ui
 cp build/libs/lj-ui.jar distribution/lj-ui.jar
